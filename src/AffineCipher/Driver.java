10084653b98fcd078908b1b9527962508d10d89c
achance6 <achance6@jhu.edu>
2019-10-20 14:05:10 -0400

    added support for upper-case input as well as fixed a bug with decoding an encoded word

--- src/AffineCipher/Driver.java
+++ src/AffineCipher/Driver.java
@@ -6,7 +6,10 @@ public final class Driver {
     private Driver() {}
 
     public static void main(String[] args) throws IOException {
-        Affine cipher = new Affine("Yeq lkcv bdk xcgk ez bdk uexlv'm qplqgwskmb.", "enable1.txt");
+        Affine cipher = new Affine("enable1.txt");
+        cipher.setPhrase("YEQ LKCV BDK XCGK EZ BDK UEXLVM QPLQGWSKMB");
+
+        //cipher.setPhrase(cipher.encode(5, 8));
         System.out.println(cipher.decode());
     }
 
