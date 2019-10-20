b2e63a0e3e8e8f6db268be9e2a53ff5a72663931
achance6 <achance6@jhu.edu>
2019-10-20 14:35:25 -0400

    Final version for git

--- src/AffineCipher/Driver.java
+++ src/AffineCipher/Driver.java
@@ -6,11 +6,7 @@ public final class Driver {
     private Driver() {}
 
     public static void main(String[] args) throws IOException {
-        Affine cipher = new Affine("enable1.txt");
-        cipher.setPhrase("YEQ LKCV BDK XCGK EZ BDK UEXLVM QPLQGWSKMB");
-
-        //cipher.setPhrase(cipher.encode(5, 8));
-        System.out.println(cipher.decode());
+        Affine cipher = new Affine("test", "enable1.txt");
     }
 
 
